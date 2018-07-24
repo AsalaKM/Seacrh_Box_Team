9a9d40ff4ab67d07eeef50d9715825b124087901
AsalaKM <asalamatarya@gmail.com>
2018-07-24 21:57:23 +0300

    Add a frontend and style

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,12 @@
 alert("Enter");
+
+const btn=  document.getElementsByClassName("rgba");
+
+btn[0].addEventListener('click',display);
+function display() {
+  var x = document.getElementsByClassName("search");
+  var y = document.getElementsByClassName("center");
+
+  x[0].setAttribute("style","visibility:visible;");
+  y[0].setAttribute("style","");
+}
