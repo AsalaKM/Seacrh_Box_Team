fc13173a8ff8370a61f86c42188aca0e52e41348
knanahassouna1 <knanahassouna2@gmail.com>
2018-07-25 09:21:50 +0300

    Add Names Json file and edit the links

--- src/function.js
+++ src/function.js
@@ -1,30 +1,29 @@
 var fs = require('fs');
 var path = require('path');
-
 function homePage(request, response) {
   response.writeHead(200, { "Content-Type": "text/html" });
 
-  fs.readFile(__dirname + '/../public/index.html', function (error, file) {
+  fs.readFile(path.join(__dirname, '..', 'public', 'index.html'), function (error, file) {
     if (error) {
       console.log(error);
       return;
     }
-
     response.end(file);
   });
 }
 
 function serveStatic(endpoint, response) {
-  console.log("Hi");
+  // type is get the type of file the request need 
   const type = endpoint.split(".")[1];
+
+  // extension is get ContentType  of file the request need 
   let extension = {
     "css": "text/css",
     "js": "application/javascript"
 
   }
   response.writeHead(200, { "Content-Type": extension[type] });
-  //const extension2=(type != html) ? '' : type;
-  //console.log(type)
+
 
   fs.readFile(path.join(__dirname, '..', endpoint), function (error, file) {
     if (error) {
@@ -40,7 +39,7 @@ function jsonFile(request, response) {
 
   response.writeHead(200, { "Content-Type": "application/json" });
 
-  fs.readFile(__dirname + '/names.json', function (error, file) {
+  fs.readFile(path.join(__dirname , 'names.json'), function (error, file) {
     if (error) {
       console.log(error);
       return;
@@ -49,11 +48,10 @@ function jsonFile(request, response) {
     response.end(file);
   });
 }
-
 function err(request, response) {
   response.writeHead(404, { "Content-Type": "text/html" });
 
-  fs.readFile(__dirname + '/../public/Error.html', function (error, file) {
+  fs.readFile(path.join(__dirname , '..', 'public', 'Error.html'), function (error, file) {
     if (error) {
       console.log(error);
       return;
@@ -62,8 +60,6 @@ function err(request, response) {
     response.end(file);
   });
 }
-
-
 module.exports = {
   homePage,
   serveStatic,
