2e80bd6fd9eb2e91b6726ffff3a5ea847d291894
donia98 <nadooshah98@gmail.com>
2018-07-25 14:36:25 +0300

    edit style and desplay cover

--- public/js/script.js
+++ public/js/script.js
@@ -1,22 +1,22 @@
-
-
-const btnStart=  document.getElementsByClassName("rgba");
-
-btnStart[0].addEventListener('click',display);
-function display() {
-  var search = document.getElementsByClassName("search");
-  var searchbtn = document.getElementsByClassName("search-btn");
-  search[0].setAttribute("style","visibility:visible;");
-  searchbtn[0].setAttribute("style","visibility:hidden;");
-}
-
-const btnEnd=  document.getElementsByClassName("end");
-
-btnEnd[0].addEventListener('click',undoDisplay);
-function undoDisplay() {
-  var search = document.getElementsByClassName("search");
-  var searchbtn = document.getElementsByClassName("search-btn");
-
-  search[0].setAttribute("style","visibility:hidden;");
-  searchbtn[0].setAttribute("style","visibility:visible;");
-}
+//
+//
+// const btnStart=  document.getElementsByClassName("rgba");
+//
+// btnStart[0].addEventListener('click',display);
+// function display() {
+//   var search = document.getElementsByClassName("search");
+//   var searchbtn = document.getElementsByClassName("search-btn");
+//   search[0].setAttribute("style","visibility:visible;");
+//   searchbtn[0].setAttribute("style","visibility:hidden;");
+// }
+//
+// const btnEnd=  document.getElementsByClassName("end");
+//
+// btnEnd[0].addEventListener('click',undoDisplay);
+// function undoDisplay() {
+//   var search = document.getElementsByClassName("search");
+//   var searchbtn = document.getElementsByClassName("search-btn");
+//
+//   search[0].setAttribute("style","visibility:hidden;");
+//   searchbtn[0].setAttribute("style","visibility:visible;");
+// }
