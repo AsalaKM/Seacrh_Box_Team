210da400af601152dab4ddcc1d4b2dbc903875f1
donia98 <nadooshah98@gmail.com>
2018-07-25 11:27:25 +0300

    edit the script in packege.json file

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var router  = require('./router');
-var port = process.env.port || 4004;
+var port = process.env.port || 4000;
 
 
 http.createServer(router).listen(port);
